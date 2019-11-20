8c522180ae3c80478564a6cc26579238198b5bc4
mortunco <tmorova15@ku.edu.tr>
2019-11-19 17:09:54 -0800

    final version

--- count-tile.sh
+++ count-tile.sh
@@ -1,9 +1,7 @@
+#!/bin/bash
 echo $1 $2
-## $1 input bam
-## $2 target library fragment bed 
+#$1 input bam
+#$2 target library fragment bed
 samtools view -b -f2 -F3332 $1 | samtools sort -@ 9 -m 10G -n - | \
 bedtools bamtobed -i - -bedpe | awk '{print $1 "\t" $2 "\t" $6 "\t" $7}' - | sort -k1,1 -k2,2n - | \
-#awk '$3-$2 < 1000 && $3 > $2 {print $0}' - | sort -k1,1 -k2,2n - | \
-bedtools intersect -wa -c -f 1 -r -a $2 -b - > fragment-counts.$1
-
-#bedtools intersect -wa -wb  -f 1 -r -a $2 -b - > fragment-counts.$1
+bedtools intersect -wa -c -f 1 -r -a $2 -b - 
--- create-tile.sh
+++ create-tile.sh
@@ -3,13 +3,6 @@
 #SBATCH --mem 720G
 #SBATCH -p long,big-mem,normal,express
 source /home/tmorova/.bashrc
-samtools view -b -F 3332 -f2 /groups/lackgrp/ll_members/tunc/phd/ana-starrseq-lncap-lacklab/analysis/mapping/starrseq-lncap-lib3-cleaned.bam | \
-samtools sort -@ 15 -m 40G -n - | \
-samtools fixmate -m -r - lncap-lib-fixmated.bam 
-samtools sort -@ 15 -m 40G lncap-lib-fixmated.bam > tmp.bam 
-samtools markdup -r tmp.bam lncap-lib-fixmated-markdupremoved.bam 
-samtools sort -@ 15 -m 40G -n lncap-lib-fixmated-markdupremoved.bam | bedtools bamtobed -i - -bedpe  > temp
-awk '{print $1 "\t" $2 "\t" $6 "\t" $7}' temp  > lib-fragments.bed
-awk '$3-$2 < 1000 && $3 > $2 {print $0}' lib-fragments.bed | sort -k1,1 -k2,2n - > tmp && mv tmp lib-fragments.bed
-bedtools intersect -a lib-fragments.bed -b /home/tmorova/STARR/coverage/ValidationRegions_ARBS-NControl-PControl.bed -wa > tmp && mv tmp lib-fragments.bed
-rm tmp.bam lncap-lib-fixmated.bam lncap-lib-fixmated-markdupremoved.bam
+module load samtools/1.9
+samtools view -h -f2 -F3332 /groups/lackgrp/ll_members/tunc/phd/ana-starrseq-lncap-lacklab/analysis/mapping/starrseq-lncap-lib3-cleaned.bam |samtools sort -@10 -m10G -n - | bedtools bamtobed -i - -bedpe > temp.bed
+cut -f 1,2,6 temp.bed | sort | uniq | bedtools intersect -a - -b /groups/lackgrp/ll_members/tunc/phd/ana-starrseq-lncap-lacklab/analysis/updated.Validation.bed -u | awk '{print $0 "\t" NR "-tile" }' - | sort -k1,1 -k2,2n  > tile.bed
